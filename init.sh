1eb8734e252d331afdd33683279640465d1cef0b
thivco <thibault.vivier@epitech.eu>
2024-04-22 01:18:16 +0200

    adding the default config and style files

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Salut mec, on relance encore son installe ? Ah, pas serieux ca..."
+
+# Install essential tools
+
+sudo pacman -S spotify
+
+# Install javascript related tools
+
+sudo pacman -S npm bun
