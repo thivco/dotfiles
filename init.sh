f566ca823b6407f0ced68610ceab7da7a1b9555e
thivco <thibault.vivier@epitech.eu>
2024-05-02 21:47:27 +0000

    Edited the init script to create directories

--- init.sh
+++ init.sh
@@ -4,15 +4,30 @@ echo "Salut mec, on relance encore son installe ? Ah, pas serieux ca..."
 
 # Install essential tools
 
-sudo pacman -S spotify
+# sudo pacman -S spotify
 
 # Install javascript related tools
 
-sudo pacman -S npm bun
+# sudo pacman -S npm bun
 
 # Config files
 # Starting with hyprland itself
 
-cp ./hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
-# ln -s ./hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+# Creating the main working directory, stuffing all of it in there to avoid clutter
+
+echo "Setting the workshop"
+mkdir -p ~/workshop/lab ~/workshop/epitech
+
+echo "Moving the dotfiles"
+mv ../dotfiles ~/workshop/lab/dotfiles
+
+echo "Cleaning up"
+rm -rf ~/dotfiles
+
+# cp ./hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+echo "Linking the hyprland.conf file"
+rm ~/.config/hypr/hyprland.conf && ln -s ./hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
 # Commented symlink as am not too sure if it works
+
+echo "Linking bashrc"
+rm ~/.bashrc && ln -s ./.bashrc ~/.bashrc && source ~/.bashrc
