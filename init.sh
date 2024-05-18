0c7c9b316597deeb46125f69fc9383ecf7ccc834
thivco <thibault.vivier@epitech.eu>
2024-05-18 16:56:08 +0200

    Added conditions to create files, added the nixOS config file automation

--- init.sh
+++ init.sh
@@ -15,8 +15,10 @@ echo "Salut mec, on relance encore son installe ? Ah, pas serieux ca..."
 
 # Creating the main working directory, stuffing all of it in there to avoid clutter
 
-echo "Setting the workshop"
-mkdir -p ~/workshop/lab ~/workshop/epitech
+if [ ! -f workshop ]; then
+    echo "Setting the workshop"
+    mkdir -p ~/workshop/lab ~/workshop/epitech
+fi
 
 echo "Moving the dotfiles"
 mv ../dotfiles ~/workshop/lab/dotfiles
@@ -29,4 +31,18 @@ echo "Linking the hyprland.conf file"
 rm ~/.config/hypr/hyprland.conf && ln -s ~/workshop/lab/dotfiles/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
 
 echo "Linking bashrc"
-rm ~/.bashrc && ln -s ~/workshop/lab/dotfiles/.bashrc ~/.bashrc && source ~/.bashrc
+rm ~/.bashrc && ln -s ~/workshop/lab/dotfiles/.bashrc ~/.bashrc 
+source ~/.bashrc
+
+# If we're using NixOS, add the config file to /etc/nixos
+if [ -f /etc/crontab ]; then
+    echo "Adding the custom config to nixOS directory"
+    sudo rm -rf ~/etc/nixos/configuration.nix && mv ~/workshop/lab/dotfiles/nixos/ /etc/nixos/configuration.nix
+    echo "Rebuild immediatly ?"
+    read rebuild_immediatly_bool
+    if [ "$rebuild_immediatly_bool" = "Y" -o "$rebuild_immediatly_bool" = "y" ]; then
+        echo "Rebuilding the nixOS config"
+        sudo nixos-rebuild switch
+    fi
+    echo "nixOS config done"
+fi
