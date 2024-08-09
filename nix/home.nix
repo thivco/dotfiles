b52f128f8c5c9fe33d58f0c46c72a80ec26a6443
thivco <thibault.vivier@epitech.eu>
2024-08-09 16:35:38 +0000

    Added wofi instead of rofi

--- nix/home.nix
+++ nix/home.nix
@@ -20,8 +20,7 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-	pkgs.hello
-
+	pkgs.wofi
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
