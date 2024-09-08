9c98de30b8af6e6fd687471bb95c5253e4afddbc
thivco <thibault.vivier@epitech.eu>
2024-09-08 01:08:34 +0000

    Added neovim to home manager

--- nix/home.nix
+++ nix/home.nix
@@ -1,66 +1,74 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "thib";
-  home.homeDirectory = "/home/thib";
+# Home Manager needs a bit of information about you and the paths it should
+# manage.
+	home.username = "thib";
+	home.homeDirectory = "/home/thib";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  programs.direnv.enable = true;
-  programs.bash.enable = true;
+	home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  nixpkgs.config.allowUnfree = true;
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-	pkgs.wofi
-	pkgs.postman
-  ];
+		programs.direnv.enable = true;
+	#programs.bash.enable = true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+	nixpkgs.config.allowUnfree = true;
+# The home.packages option allows you to install Nix packages into your
+# environment.
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+	home.packages = [
+		pkgs.wofi
+			pkgs.postman
+	];
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/thib/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+# Home Manager is pretty good at managing dotfiles. The primary way to manage
+# plain files is through 'home.file'.
+	home.file = {
+	};
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+
+# Home Manager can also manage your environment variables through
+# 'home.sessionVariables'. These will be explicitly sourced when using a
+# shell provided by Home Manager. If you don't want to manage your shell
+# through Home Manager then you have to manually source 'hm-session-vars.sh'
+# located at either
+#
+#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  /etc/profiles/per-user/thib/etc/profile.d/hm-session-vars.sh
+#
+
+	programs.neovim = {
+		enable = true;
+		viAlias = true;
+		vimAlias = true;
+		vimdiffAlias = true;
+
+		plugins = with pkgs.vimPlugins; [
+			nvim-lspconfig
+				telescope-nvim
+				telescope-fzf-native-nvim
+				(nvim-treesitter.withPlugins (p: [
+							      p.tree-sitter-nix
+							      p.tree-sitter-vim
+							      p.tree-sitter-bash
+							      p.tree-sitter-json
+				]))
+
+		vim-nix
+		];
+	};
+
+	home.sessionVariables = {
+# EDITOR = "emacs";
+	};
+
+# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -161,6 +161,7 @@
      
      # Browsing
      
+     chromium
      # Dev
      vscode
      go
